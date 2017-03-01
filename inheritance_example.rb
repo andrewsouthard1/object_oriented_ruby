395c6912be183678a3c5e1f2fdb954e2cb8e6aae
Andrew <andrewsouthard111@gmail.com>
2017-02-28 21:18:38 -0500

    Change inheritance to module

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,11 +1,4 @@
-class Vehicle
-  attr_reader :direction, :brake
-
-  def initialize(input_options = {})
-    @speed = 0
-    @direction = "north"
-  end
-
+module Movable
   def accelerate
     @speed += 10
   end
@@ -15,11 +8,13 @@ class Vehicle
   end
 end
 
-class Car < Vehicle
-  attr_reader :fuel, :make, :model
+class Car
+  attr_reader :fuel, :make, :model, :speed, :direction
+  include Movable
 
   def initialize(input_options)
-    super
+    @speed = 0
+    @direction = "north"
     @fuel = input_options[:fuel]
     @make = input_options[:make]
     @model = input_options[:model]
@@ -30,12 +25,13 @@ class Car < Vehicle
   end
 end
 
-class Bike < Vehicle
-  attr_reader :speed, :type, :weight
+class Bike
+  attr_reader :speed, :type, :weight, :direction
+  include Movable
 
   def initialize(input_options)
-    super
-    @speed = input_options[:speed]
+    @speed = 0
+    @direction = "north"
     @type = input_options[:type]
     @weight = input_options[:weight]
   end
@@ -46,10 +42,11 @@ class Bike < Vehicle
 end
 
 car1 = Car.new(fuel: "Electric", make: "Tesla", model: "Model 3")
-car1.honk_horn
-p car1.model
+# car1.honk_horn
+# p car1.model
 
-bike1 = Bike.new(speed: "fixed", type: "hybrid", weight: 20)
-bike1.ring_bell
+bike1 = Bike.new(type: "hybrid", weight: 20)
+# bike1.ring_bell
 p bike1.speed
-
+bike1.accelerate
+p bike1.speed
